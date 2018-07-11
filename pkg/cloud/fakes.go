ab27a69efe968618527340eb816a8118ce7d8ca5
Fabio Bertinatto <fabiojrb@gmail.com>
2018-07-11 08:49:48 +0200

    Improve fake cloud provider

--- pkg/cloud/fakes.go
+++ pkg/cloud/fakes.go
@@ -1,5 +1,14 @@
 package cloud
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
 type FakeCloudProvider struct {
 	disks map[string]*DiskOptions
 }
@@ -11,21 +20,29 @@ func NewFakeCloudProvider() *FakeCloudProvider {
 }
 
 func (f *FakeCloudProvider) CreateDisk(diskOptions *DiskOptions) (volumeID VolumeID, err error) {
-	//r1 := rand.New(rand.NewSource(time.Now().UnixNano()))
-	//volID := fmt.Sprintf("vol-%d", r1.Uint64())
-	//f.disks[volID] = diskOptions
-	//return VolumeID(volID), nil
-	return VolumeID("vol-test"), nil
+	r1 := rand.New(rand.NewSource(time.Now().UnixNano()))
+	volID := fmt.Sprintf("vol-%d", r1.Uint64())
+	f.disks[volID] = diskOptions
+	return VolumeID(volID), nil
 }
 
 func (f *FakeCloudProvider) DeleteDisk(volumeID VolumeID) (bool, error) {
-	//volID := string(volumeID)
-	//if _, ok := f.disks[volID]; !ok {
-	//return false, status.Error(codes.NotFound, "")
-	//}
-	//delete(f.disks, volID)
+	volID := string(volumeID)
+	if _, ok := f.disks[volID]; !ok {
+		return false, status.Error(codes.NotFound, "")
+	}
+	delete(f.disks, volID)
 	return true, nil
 }
+
 func (f *FakeCloudProvider) GetVolumesByTagName(tagKey, tagVal string) ([]string, error) {
-	return nil, nil
+	var results []string
+	for volID, opts := range f.disks {
+		for key, value := range opts.Tags {
+			if key == tagKey && value == tagVal {
+				results = append(results, volID)
+			}
+		}
+	}
+	return results, nil
 }
