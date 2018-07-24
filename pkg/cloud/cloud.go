09133430462851f6a8bdac93fb10132ff1dde1d1
Fabio Bertinatto <fabiojrb@gmail.com>
2018-07-24 14:45:59 +0200

    Move default values to cloud.go

--- pkg/cloud/cloud.go
+++ pkg/cloud/cloud.go
@@ -15,6 +15,36 @@ import (
 	"github.com/bertinatto/ebs-csi-driver/pkg/util"
 )
 
+const (
+	// TODO: what should be the default size?
+	// DefaultVolumeSize represents the default volume size.
+	DefaultVolumeSize int64 = 1 * 1024 * 1024 * 1024
+
+	// VolumeNameTagKey is the key value that refers to the volume's name.
+	VolumeNameTagKey = "com.amazon.aws.csi.volume"
+
+	// VolumeTypeIO1 represents a provisioned IOPS SSD type of volume.
+	VolumeTypeIO1 = "io1"
+
+	// VolumeTypeGP2 represents a general purpose SSD type of volume.
+	VolumeTypeGP2 = "gp2"
+
+	// VolumeTypeSC1 represents a cold HDD (sc1) type of volume.
+	VolumeTypeSC1 = "sc1"
+
+	// VolumeTypeST1 represents a throughput-optimized HDD type of volume.
+	VolumeTypeST1 = "st1"
+
+	// MinTotalIOPS represents the minimum Input Output per second.
+	MinTotalIOPS int64 = 100
+
+	// MaxTotalIOPS represents the maximum Input Output per second.
+	MaxTotalIOPS int64 = 20000
+
+	// DefaultVolumeType specifies which storage to use for newly created Volumes.
+	DefaultVolumeType = VolumeTypeGP2
+)
+
 type CloudProvider interface {
 	CreateDisk(string, *DiskOptions) (*Disk, error)
 	DeleteDisk(string) (bool, error)
