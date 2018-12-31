142d7ee7e67f6ca1a8a4839a255ff5c8de087aa0
Shane Siebken <shane.siebken@gmail.com>
2018-12-31 15:54:07 -0500

    add script to generate tiller tls assets

--- tiller/tls/do_tls.sh
+++ tiller/tls/do_tls.sh
@@ -6,3 +6,5 @@ openssl genrsa -out ./helm.key.pem 4096
 
 openssl req -key tiller.key.pem -new -sha256 -out tiller.csr.pem
 openssl req -key helm.key.pem -new -sha256 -out helm.csr.pem
+
+openssl x509 -req -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -in tiller.csr.pem -out tiller.cert.pem -days 365
