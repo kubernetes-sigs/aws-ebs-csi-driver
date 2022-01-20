cf6fa4f5e33f18deb14aa1bb0b6ce5c5cec41420
Hunter Morris <4218+hntrmrrs@users.noreply.github.com>
2022-01-20 12:22:32 +0000

    Add --az-from-metadata flag to controller

--- cmd/options/controller_options.go
+++ cmd/options/controller_options.go
@@ -35,6 +35,8 @@ type ControllerOptions struct {
 	KubernetesClusterID string
 	// flag to enable sdk debug log
 	AwsSdkDebugLog bool
+	// flag to enable metadata service for default topology
+	AvailabilityZoneFromMetadata bool
 }
 
 func (s *ControllerOptions) AddFlags(fs *flag.FlagSet) {
@@ -42,4 +44,5 @@ func (s *ControllerOptions) AddFlags(fs *flag.FlagSet) {
 	fs.Var(cliflag.NewMapStringString(&s.ExtraVolumeTags), "extra-volume-tags", "DEPRECATED: Please use --extra-tags instead. Extra volume tags to attach to each dynamically provisioned volume. It is a comma separated list of key value pairs like '<key1>=<value1>,<key2>=<value2>'")
 	fs.StringVar(&s.KubernetesClusterID, "k8s-tag-cluster-id", "", "ID of the Kubernetes cluster used for tagging provisioned EBS volumes (optional).")
 	fs.BoolVar(&s.AwsSdkDebugLog, "aws-sdk-debug-log", false, "To enable the aws sdk debug log level (default to false).")
+	fs.BoolVar(&s.AvailabilityZoneFromMetadata, "az-from-metadata", false, "If true, use the metadata service to fetch controller AZ. Otherwise, default to a random one in the region.")
 }
