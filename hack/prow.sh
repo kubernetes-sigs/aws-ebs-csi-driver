506ef9a7eec70ff9db912f051da7a814cae1d1a6
Connor Catlett <conncatl@amazon.com>
2022-12-16 16:34:42 +0000

    Revert to grabbing tag from PULL_BASE_REF in prow.sh
    Signed-off-by: Connor Catlett <conncatl@amazon.com>

--- hack/prow.sh
+++ hack/prow.sh
@@ -41,5 +41,5 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 loudecho "Push manifest list containing amazon linux and windows based images to GCR"
 export REGISTRY=$REGISTRY_NAME
 export TAG=$GIT_TAG
-export VERSION=$(git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD)
+export VERSION=$PULL_BASE_REF
 IMAGE=gcr.io/k8s-staging-provider-aws/aws-ebs-csi-driver make all-push
