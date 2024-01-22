1a66420c8fa649a345fa5a943d8f367358291251
Connor Catlett <conncatl@amazon.com>
2024-01-22 16:54:23 +0000

    PROW TEST DO NOT MERGE
    Signed-off-by: Connor Catlett <conncatl@amazon.com>

--- hack/prow.sh
+++ hack/prow.sh
@@ -30,9 +30,9 @@ gcloud auth configure-docker
 loudecho "Set up Docker Buildx"
 # See https://github.com/docker/setup-buildx-action
 # and https://github.com/kubernetes-csi/csi-release-tools/blob/master/build.make#L132
-export DOCKER_CLI_EXPERIMENTAL=enabled
-trap "docker buildx rm multiarchimage-buildertest" EXIT
-docker buildx create --use --name multiarchimage-buildertest
+# export DOCKER_CLI_EXPERIMENTAL=enabled
+# trap "docker buildx rm multiarchimage-buildertest" EXIT
+# docker buildx create --use --name multiarchimage-buildertest
 
 loudecho "Set up QEMU"
 # See https://github.com/docker/setup-qemu-action
