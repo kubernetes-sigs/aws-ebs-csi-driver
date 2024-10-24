84977af7c27dba8f521f1315297d2b56f55960eb
Connor Catlett <conncatl@amazon.com>
2024-10-24 13:51:00 +0000

    Add middleware to log server errors
    Signed-off-by: Connor Catlett <conncatl@amazon.com>

--- pkg/cloud/handlers.go
+++ pkg/cloud/handlers.go
@@ -22,6 +22,7 @@ import (
 	"time"
 
 	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
+	"github.com/aws/aws-sdk-go-v2/aws/retry"
 	"github.com/aws/smithy-go"
 	"github.com/aws/smithy-go/middleware"
 	"github.com/kubernetes-sigs/aws-ebs-csi-driver/pkg/metrics"
@@ -54,6 +55,32 @@ func RecordRequestsMiddleware() func(*middleware.Stack) error {
 			} else {
 				duration := time.Since(start).Seconds()
 				metrics.Recorder().ObserveHistogram("cloudprovider_aws_api_request_duration_seconds", duration, labels, nil)
+			}
+			return output, metadata, err
+		}), middleware.After)
+	}
+}
+
+// LogServerErrorsMiddleware is a middleware that logs server errors received when attempting to contact the AWS API
+// A specialized middleware is used instead of the SDK's built-in retry logging to allow for customizing the verbosity
+// of throttle errors vs server/unknown errors, to prevent flooding the logs with throttle errors
+func LogServerErrorsMiddleware() func(*middleware.Stack) error {
+	return func(stack *middleware.Stack) error {
+		return stack.Finalize.Add(middleware.FinalizeMiddlewareFunc("LogServerErrorsMiddleware", func(ctx context.Context, input middleware.FinalizeInput, next middleware.FinalizeHandler) (output middleware.FinalizeOutput, metadata middleware.Metadata, err error) {
+			output, metadata, err = next.HandleFinalize(ctx, input)
+			if err != nil {
+				var apiErr smithy.APIError
+				if errors.As(err, &apiErr) {
+					if _, isThrottleError := retry.DefaultThrottleErrorCodes[apiErr.ErrorCode()]; isThrottleError {
+						// Only log throttle errors under a high verbosity as we expect to see many of them
+						// under normal bursty/high-TPS workloads
+						klog.V(6).ErrorS(apiErr, "Throttle error from AWS API")
+					} else {
+						klog.V(4).ErrorS(apiErr, "Error from AWS API")
+					}
+				} else {
+					klog.V(4).ErrorS(err, "Unknown error attempting to contact AWS API")
+				}
 			}
 			return output, metadata, err
 		}), middleware.After)
