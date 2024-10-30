39efb4eecb52d7502e5106fde9b31db1e811ef21
Drew Sirenko <68304519+AndrewSirenko@users.noreply.github.com>
2024-10-29 21:31:58 -0400

    cleanup: enable goimports linter

--- tests/e2e/testsuites/pre_provisioned_reclaim_policy_tester.go
+++ tests/e2e/testsuites/pre_provisioned_reclaim_policy_tester.go
@@ -16,7 +16,7 @@ package testsuites
 
 import (
 	"github.com/kubernetes-sigs/aws-ebs-csi-driver/tests/e2e/driver"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	clientset "k8s.io/client-go/kubernetes"
 )
 
