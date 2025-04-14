508341221a9554629694edacd05b6b1aa923f835
Rafael Mendez <mdzraf@amazon.com>
2025-04-14 17:41:02 +0000

    Bumping Dependencies Ahead of 1.42.0 release

--- hack/tools/install.sh
+++ hack/tools/install.sh
@@ -19,19 +19,19 @@ set -euo pipefail
 readonly PKG_ROOT="$(git rev-parse --show-toplevel)"
 
 # https://github.com/aws/aws-cli/tags
-AWSCLI_VERSION="2.24.22"
+AWSCLI_VERSION="2.26.1"
 # https://github.com/helm/chart-testing
 CT_VERSION="v3.12.0"
 # https://github.com/eksctl-io/eksctl
-EKSCTL_VERSION="v0.205.0"
+EKSCTL_VERSION="v0.207.0"
 # https://github.com/onsi/ginkgo
-GINKGO_VERSION="v2.23.0"
+GINKGO_VERSION="v2.23.4"
 # https://github.com/golangci/golangci-lint
-GOLANGCI_LINT_VERSION="v1.64.7"
+GOLANGCI_LINT_VERSION="v1.64.8"
 # https://github.com/hairyhenderson/gomplate
 GOMPLATE_VERSION="v4.3.1"
 # https://github.com/helm/helm
-HELM_VERSION="v3.17.1"
+HELM_VERSION="v3.17.3"
 # https://github.com/kubernetes/kops
 # Commit is preferred over version if non-empty, and can
 # be used to test new Kubernetes releases earlier
@@ -46,7 +46,7 @@ SHFMT_VERSION="v3.11.0"
 # https://pypi.org/project/yamale/
 YAMALE_VERSION="6.0.0"
 # https://pypi.org/project/yamllint/
-YAMLLINT_VERSION="1.36.0"
+YAMLLINT_VERSION="1.37.0"
 
 OS="$(go env GOHOSTOS)"
 ARCH="$(go env GOHOSTARCH)"
