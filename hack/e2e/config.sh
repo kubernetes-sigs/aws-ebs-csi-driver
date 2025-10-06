baae5d4427516292d20af69bd90f4da0ef52ffc3
Eddie Torres <torredil@amazon.com>
2025-10-06 15:51:57 +0000

    Upgrade CI to Kubernetes 1.34
    Signed-off-by: Eddie Torres <torredil@amazon.com>

--- hack/e2e/config.sh
+++ hack/e2e/config.sh
@@ -42,8 +42,8 @@ FIPS_TEST=${FIPS_TEST:-"false"}
 
 # kops: must include patch version (e.g. 1.19.1)
 # eksctl: mustn't include patch version (e.g. 1.19)
-K8S_VERSION_KOPS=${K8S_VERSION_KOPS:-1.33.3}
-K8S_VERSION_EKSCTL=${K8S_VERSION_EKSCTL:-1.33}
+K8S_VERSION_KOPS=${K8S_VERSION_KOPS:-1.34.1}
+K8S_VERSION_EKSCTL=${K8S_VERSION_EKSCTL:-1.34}
 
 # Override AMI - eksctl clusters only
 LINUX_AMI=${LINUX_AMI:-}
